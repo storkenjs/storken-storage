610ffb19913d07bef3b5661ee1eedb5914443bab
Kerem Noras <kerem@noras.tech>
2022-04-05 15:10:20 +0300

    feat: Support types

--- src/storken-storage.ts
+++ src/storken-storage.ts
@@ -1,22 +1,38 @@
-export const StorkenStorage = (storken, generalStorage) => {
-  const { storage: specificStorage, disableStorageOnMount, disableSetterOnStorage } = storken.opts
+import { IOptions, Storken } from 'storken'
+
+export interface IFuncs {
+  setFromStorage: () => Promise<any>,
+  saveToStorage: (value: any) => Promise<any> | undefined,
+  clear: (key: string, remove: boolean, reset: boolean) => Promise<void> | undefined,
+  multiClear: (...keys: string[]) => Promise<void>
+}
+
+export interface AsyncStorage {
+  getItem(key: string): PromiseLike<string | null>,
+  setItem(key: string, value: string): PromiseLike<void>,
+  removeItem(key: string): PromiseLike<void>
+}
+
+export const StorkenStorage = (storken: Storken<any>, generalStorage: Storage | AsyncStorage) => {
+  const { storage: specificStorage, disableStorageOnMount, disableSetterOnStorage }: IOptions & {
+    storage?: Storage,
+    disableStorageOnMount?: boolean,
+    disableSetterOnStorage?: boolean
+  } = storken.opts
   const storage = specificStorage || generalStorage
 
-  const Funcs = {
+  const Funcs: IFuncs = {
     setFromStorage: async () => {
       storken.load(true)
       storken.dispatchEvent('loadingStorage')
       try {
-        let stored = storage.getItem(storken.namespace + storken.key)
+        const stored = await storage.getItem((storken.namespace as string) + (storken.key as string))
+        let originalStored: any = null
         if (stored !== null) {
-          if (typeof stored?.then === 'function') {
-            stored = await stored
-          }
-
           storken.dispatchEvent('loadedStorage', stored)
 
           if (stored) {
-            stored = JSON.parse(stored)
+            originalStored = JSON.parse(stored)
             storken.dispatchEvent('settingFromStorage', stored)
             storken.set(stored, { disableSetter: disableSetterOnStorage || true })
             storken.dispatchEvent('settedFromStorage', stored)
@@ -24,17 +40,19 @@ export const StorkenStorage = (storken, generalStorage) => {
 
           storken.load(false)
 
-          return stored
+          return originalStored
         }
       } catch (err) {
         console.warn('Storken: failed to set from storage', { value: storken.value, err })
       }
 
       storken.load(false)
+
+      return undefined
     },
     saveToStorage: async (value) => {
       storken.dispatchEvent('savingToStorage')
-      const address = storken.namespace + storken.key
+      const address = storken.namespace + (storken.key as string)
       const val = JSON.stringify(value)
       try {
         await storage.setItem(address, val)
@@ -43,6 +61,8 @@ export const StorkenStorage = (storken, generalStorage) => {
       } catch (err) {
         console.warn('Storken: failed to setItem', { value, err })
       }
+
+      return undefined
     },
     clear: async (key, remove, reset = true) => {
       storken.dispatchEvent('removingStorage')
@@ -52,13 +72,13 @@ export const StorkenStorage = (storken, generalStorage) => {
       storken.dispatchEvent('removedStorage')
 
       if (remove) {
-        storken.remove(key, reset)
+        storken.Store.remove(key, reset)
       }
     },
-    multiClear: async (...keys) => {
+    multiClear: async (...keys: string[]) => {
       for (const keyIndex in keys) {
         const key = keys[keyIndex]
-        await storken.clear(key)
+        await storken.Store.remove(key)
       }
     }
   }
@@ -66,12 +86,12 @@ export const StorkenStorage = (storken, generalStorage) => {
   Object.assign(storken, Funcs)
 
   if (storage && !disableStorageOnMount) {
-    storken.setFromStorage()
+    (storken as typeof storken & IFuncs).setFromStorage()
   }
 
-  storken.on('set', (newValue) => {
+  storken.on('set', (newValue: any) => {
     if (storage) {
-      storken.saveToStorage(newValue)
+      (storken as typeof storken & IFuncs).saveToStorage(newValue)
     }
   })
 
